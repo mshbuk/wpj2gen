1d6b54ca37ecfd9915e1f2e98e31b5dc89b20fe5
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2022-01-28 12:39:32 +0200

    no message

--- src/exam/Option.java
+++ src/exam/Option.java
@@ -1,5 +1,32 @@
 package exam;
 
 public class Option<T> {
-    // TODO
+
+    private T value;
+    private boolean isEmpty;
+
+    private Option(T value) {
+        isEmpty = true;
+        this.value = value;
+    }
+
+    private Option() {
+        isEmpty = true;
+    }
+
+    static <T> Object some(T value) {
+        Option object = new Option(value);
+        return object;
+    }
+
+    static <T> void none() {
+        Option object = new Option(null);
+    }
+
+    public Option get() throws EmptyOptionException {
+        if isEmpty == true {
+            throw EmptyOptionException;
+        }
+        return value;
+    }
 }
