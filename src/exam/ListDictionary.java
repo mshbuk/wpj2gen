006726a995eca341546078b6755bc4cc19313502
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2022-01-28 12:56:13 +0200

    no message

--- src/exam/ListDictionary.java
+++ src/exam/ListDictionary.java
@@ -1,5 +1,49 @@
 package exam;
 
-public class ListDictionary<K, V>  {
-   // TODO
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ListDictionary<K, V> implements Dictionary {
+    List<K> keys;
+    List<V> values;
+
+    @Override
+    public Option get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object get(Object key, Object defaultValue) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean put(Object key, Object value) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Object key, Object value) {
+        return false;
+    }
+
+    @Override
+    public void clear(Object key) {
+
+    }
+
+    @Override
+    public Stream keyStream() {
+        return null;
+    }
+
+    @Override
+    public Stream valueStream() {
+        return null;
+    }
 }
--- src/exam/SetDictionary.java
+++ src/exam/SetDictionary.java
@@ -1,5 +1,46 @@
 package exam;
 
-public class SetDictionary<K, V>  {
-    // TODO
+import java.util.stream.Stream;
+
+public class SetDictionary<K, V> implements Dictionary{
+
+    @Override
+    public Option get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object get(Object key, Object defaultValue) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean put(Object key, Object value) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Object key, Object value) {
+        return false;
+    }
+
+    @Override
+    public void clear(Object key) {
+
+    }
+
+    @Override
+    public Stream keyStream() {
+        return null;
+    }
+
+    @Override
+    public Stream valueStream() {
+        return null;
+    }
 }
